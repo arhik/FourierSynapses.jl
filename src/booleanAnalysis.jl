839847721e015f11596bb844905f8d44b4711c3c
arhik <arhik23@gmail.com>
2022-10-11 22:11:06 +0530

    toBits and toSpins consistency

--- src/booleanAnalysis.jl
+++ src/booleanAnalysis.jl
@@ -1,21 +1,27 @@
 using LinearAlgebra
 using Zygote
 using Optimisers
+using BenchmarkTools
 
-a = BitVector(rand(Bool, 2^16))
-b = BitVector(rand(Bool, 2^16))
+a = BitVector(rand(Bool, 2^4))
+b = BitVector(rand(Bool, 2^4))
 
 c = a * (adjoint(b))
+
+toSpins(a::Bool) = @inline toSpins(Val(a))
+@inline toSpins(a::Val{true}) = -1
+@inline toSpins(a::Val{false}) = +1
+toSpins(a::BitVector) = toSpins.(a)
+
+toBits(a::Number) = a > 0
+toBits(a::AbstractArray) = toBits.(ifwht(a))
+
 
 using FFTW
 using Hadamard
 
-frombits(a::BitVector) = begin
-    [((bit == true) ? (+1) : (-1)) |> Int8 for bit in a]
-end
-
-aCoeffs = fwht(a |> frombits)
-bCoeffs = fwht(b |> frombits)
+aCoeffs = fwht(a |> toSpins)
+bCoeffs = fwht(b |> toSpins)
 
 d = dot(aCoeffs, bCoeffs)
 
@@ -23,25 +29,15 @@ gradient(dot, aCoeffs, bCoeffs)
 
 opt = Descent(0.01f0)
 
-@btime for epoch in 1:100
+for epoch in 1:100
 	grads = gradient((x, y) -> -dot(x, y), aCoeffs, bCoeffs)
 	aCoeffs .-= grads[1]*opt.eta
 	# bCoeffs .-= grads[2]*opt.eta
 	aCoeffs .= aCoeffs/norm(aCoeffs)
 	# bCoeffs .= bCoeffs/norm(bCoeffs)
 	# bCoeffs .-= grads[2]*opt.eta
-	@info dot(aCoeffs, bCoeffs)
-	@
 end
 
-toSpin(a::Bool) = @inline toSpin(Val(a))
-@inline toSpin(a::Val{true}) = -1
-@inline toSpin(a::Val{false}) = +1
-
-toBits(a::Number) = a > 0
-toBits(a::AbstractArray) = toBits.(ifwht(a))
-
-# broadcast(toSpin, a)
 
 struct Segment{T}
     a::BitVector # TODO keep track of when bit flips
